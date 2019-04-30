de54cb02357efc86a4a6f344b2d5b7ef2645d2e2
Rodion <markov-r@mail.ua>
2019-04-30 18:43:40 +0400

    initial commit

---
+++ target/SQLVisualization-1.0-SNAPSHOT/js/js_for_creator.js
@@ -0,0 +1,20 @@
+$('#sendId').on('click', function(){
+    // var data = [], id;
+    var data = [];
+    // for( id in basket) {
+    //     data.push({
+    //       id: id,
+    //       qty: basket[id]
+    //     });
+    // }
+    $.ajax({
+        url: '//CreateOneTable.java', // url куда отправлять
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: function(response){
+            alert("Hello");
+        },
+        processData: false
+    });
+});
